f4e79ea97ae33cb17823c08629b86c7b7f735868
NiiseKai-01 <larrysmithg97@gmail.com>
2025-09-30 03:01:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',   // 👈 important for Vercel (ensures CSS/JS load correctly)
 })
